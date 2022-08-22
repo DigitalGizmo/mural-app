f09ae64d957829b7da905697cf5003718e1a0f7f
Juliet Jacobson <julietanna@users.noreply.github.com>
2022-08-22 17:02:14 -0400

    slide nav improvements-not done yet

--- src/components/pops/Slide.js
+++ src/components/pops/Slide.js
@@ -15,10 +15,25 @@ const Slide = ({popData}) => {
   return (
     <div>
       <div className="pop-img">
+
+      <nav className="slide-nav prev-slide">
+        { slideIndex > 0 
+          ? <a href="/" onClick={ prevSlide }>&larr;</a> 
+          : <span className="no-more">&larr;</span>
+        }
+      </nav>
+
         {/* <img src="https://dev.digitalgizmo.com/mural-assets/pops/learnmore/fullpics/' %}p{{ slide.learnmore.article.panel.ordinal }}-{{ slide.learnmore.article.article_type }}-{{ slide.learnmore.learnmore_type }}-{{ slide.slide_num }}.jpg"/> */}
         <img src={`https://dev.digitalgizmo.com/mural-assets/pops/learnmore/fullpics/p${popData.panelNum}-${popData.articleType}-${popData.learnmoreNode.learnmoreType}-${slideIndex+1}.jpg`}
         alt={popData.learnmoreNode.title} />
-    
+
+      <nav className="slide-nav next-slide">
+        { slideIndex < (popData.learnmoreNode.slideSet.edges.length -1)
+          ? <a href="/" onClick={ nextSlide }>&rarr;</a>
+          : <span className="no-more">&rarr;</span>
+        }
+      </nav>
+
         {/* title is included in the caption */}
 
         <div dangerouslySetInnerHTML={{ __html: popData.learnmoreNode.slideSet.edges[slideIndex].node.caption}}/>
@@ -27,23 +42,6 @@ const Slide = ({popData}) => {
       <h4 dangerouslySetInnerHTML={{ __html: popData.learnmoreNode.slideSet.edges[slideIndex].node.title}}
       />    
       <div dangerouslySetInnerHTML={{ __html: popData.learnmoreNode.slideSet.edges[slideIndex].node.narrative}}/>
-
-      <nav className="slide-nav">
-        <ul>
-          <li>
-            { slideIndex > 0 
-              ? <a href="/" onClick={ prevSlide }>&larr; Previous</a> 
-              : <span> &larr; Previous</span>
-            }
-            {" "}|{" "}
-            { slideIndex < (popData.learnmoreNode.slideSet.edges.length -1)
-              ? <a href="/" onClick={ nextSlide }>Next &rarr;</a>
-              : <span>Next &rarr;</span>
-            }
-          </li>
-        </ul>
-      </nav>
-
 
     </div>
   )
