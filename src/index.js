6ec5be08eb2cb778053df89fd6d32cc660c46f1c
Don Button <donpublic@digitalgizmo.com>
2022-09-06 11:01:14 -0400

    remove relative home for now

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
+      {/* <BrowserRouter basename='/mural-relative'> */}
       <BrowserRouter>
       {/* <GlobalProvider> */}
           <Routes>
