16b1c1420b41bf71669ecf926a35ce559694db8a
Don Button <donpublic@digitalgizmo.com>
2022-09-15 17:10:34 -0400

    change slide narrative to fade

--- src/components/pops/Slide.js
+++ src/components/pops/Slide.js
@@ -48,15 +48,17 @@ const Slide = ({popData}) => {
       <motion.div
         className="slide-container"
         key={slideIndex}     
-        initial={{ x: direction === 1 ? '100%' : '-100%'}}
-        animate={{ x: 0, opacity: 1, transition: {  duration: 1 } }}
-        exit={{x: direction === 1 ? '-100%' : '100%', 
-          transition: {  duration: 1 }
-        }}
-        // exit={{opacity: 0.2, transition: {duration: 0.5}}}
-        {...bindSlide()}
+        {...bindSlide()}          
       >
-        <div className="pop-img">
+        <motion.div 
+          className="pop-img"
+          initial={{ x: direction === 1 ? '100%' : '-100%'}}
+          animate={{ x: 0, opacity: 1, transition: {  duration: 1 } }}
+          exit={{x: direction === 1 ? '-100%' : '100%', 
+            transition: {  duration: 1 }
+          }}
+          // exit={{opacity: 0.2, transition: {duration: 0.5}}}
+        >
 
           <motion.nav 
             className="slide-nav prev-slide"
@@ -104,14 +106,14 @@ const Slide = ({popData}) => {
 
           <div dangerouslySetInnerHTML={{ __html: 
             popData.learnmoreNode.slideSet.edges[slideIndex].node.caption}}/>
-        </div>
+        </motion.div>
       
         <motion.div 
           className="slide-caption"
           key={popData.learnmoreNode.slideSet.edges[slideIndex].node.title}
-          initial={{ opacity: 0.2}}
+          initial={{ opacity: 0}}
           animate={{ opacity: 1}}
-          exit={{ opacity: 0.2}}
+          exit={{ opacity: 0}}
           transition={{duration: 0.7}}
         >  
           <h4 dangerouslySetInnerHTML={{ __html: 
